200d9dc2803c969c13bf0d89c8a6012382ee13cf
RM02 <rafamark244@gmail.com>
2021-01-22 16:13:16 -0400

    server.js file fixed

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express()
 
-app.use(express.static(__dirname + '/dist/dictionary-app'));
+app.use(express.static(__dirname + './front-end/dist/'));
 
-app.get('/*', function(req, res) {
-	res.sendFile(path.join(__dirname + "/dist/dictionary-app/index.html"));
+app.get('*', function(req, res) {
+	res.sendFile(path.join(__dirname + "./front-end/dist/index.html"));
 });
 
 app.listen(process.env.PORT || 8080);
