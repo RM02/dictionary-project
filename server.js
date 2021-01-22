7b69d378f8fe783aafe1be223a9ade5023a207bd
RM02 <rafamark244@gmail.com>
2021-01-22 16:02:00 -0400

    'heroku'

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express()
+
+app.use(express.static(__dirname + '/dist/dictionary-app'));
+
+app.get('/*', function(req, res) {
+	res.sendFile(path.join(__dirname + "/dist/dictionary-app/index.html"));
+});
+
+app.listen(process.env.PORT || 8080);
