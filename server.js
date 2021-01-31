7062268ea74a2b506f1bb5a7c285ae69ba5fc248
RM02 <rafamark244@gmail.com>
2021-01-30 23:06:08 -0400

    'heroku'

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/dictionary-online-app'));
+app.use(express.static(__dirname + '/dist/dictionary-online-app/'));
 
 app.get('/*', function(req,res) {
     
